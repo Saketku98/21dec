50863f8d63725225aa696dbb8c6407e30d62f094
Saketku98 <88224770+Saketku98@users.noreply.github.com>
2022-01-01 23:36:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let num1=parseFloat(prompt("Enter 1st Number "))
 let num2=parseFloat(prompt("Enter 2nd Number "))
 let num3=parseFloat(prompt("Enter 3rd Number "))
 
-console.log(Math.min(numb1,numb2,numb3))
-alert("The Minimum of three Number is " + Math.min(numb1,numb2,numb3) )
+console.log(Math.min(num1,num2,num3))
+alert("The Minimum of three Number is " + Math.min(num1,num2,num3) )
